d8e1b3395720c445f8354ddbad41755c69b70df6
sanchaymittal <sanchaymittal@gmail.com>
2020-03-10 13:30:06 +0530

    Update image dir.

--- src/data/users.js
+++ src/data/users.js
@@ -4,13 +4,14 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const users = [
   // Please add in alphabetical order of title.
   {
     title: 'Pocket Full of Quarters',
     description: 'Pocketful of Quarters is the universal cryptocurrency for games! The Quarters team will be using Matic Network to ensure instant and low-fee transactions in-games that use the Quarters token.',
-    preview: require('../../img/showcase/quarters.png'),
+    preview: useBaseUrl('../../img/showcase/quarters.png'),
     website: 'https://www.pocketfulofquarters.com/',
     fbOpenSource: false,
     pinned: false,
@@ -18,7 +19,7 @@ const users = [
   {
     title: 'Plentix',
     description: 'Plentix is a decentralized platform aiming to transform the referral economy. The Plentix team will be using Matic Network to ensure prompt referral payouts on their platform.',
-    preview: require('../../img/showcase/plentix.png'),
+    preview: useBaseUrl('../../img/showcase/plentix.png'),
     website: 'https://plentix.io/',
     fbOpenSource: false,
     pinned: false,
@@ -26,7 +27,7 @@ const users = [
   {
     title: 'BankIT',
     description: 'BankIT plans to store fiat transactions of their banking network on the blockchain, to provide immutable proof of transactions. Matic Network was chosen because of our awesome scalability features.',
-    preview: require('../../img/showcase/bankit.png'),
+    preview: useBaseUrl('../../img/showcase/bankit.png'),
     website: 'https://www.bankit.in/',
     fbOpenSource: false,
     pinned: true,
@@ -34,7 +35,7 @@ const users = [
   {
     title: 'CryptoControl',
     description: 'CryptoControl is a crypto news, data aggregator and trading platform used by top crypto traders and investors. CryptoControl will use Matic Network to help provide traders with faster settlements at lower rates.',
-    preview: require('../../img/showcase/cryptocontrol.png'),
+    preview: useBaseUrl('../../img/showcase/cryptocontrol.png'),
     website: 'https://cryptocontrol.io/en/',
     fbOpenSource: false,
     pinned: true,
