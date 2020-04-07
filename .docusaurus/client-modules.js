c7872d4e696c21626e5361055f1cb307e185a45c
Anurag Arjun <anurag@matic.network>
2020-04-07 10:27:17 +0530

    Removed .docusaurus from repositiory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/anurag-a/code/matic/matic-docs/src/css/custom.css"),
-  require("/Users/anurag-a/code/matic/matic-docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
