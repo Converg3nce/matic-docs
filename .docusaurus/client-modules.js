15b57cf083db7ab9b44b2ca9539a969608a1b264
sanchaymittal <sanchaymittal@gmail.com>
2020-04-07 16:50:30 +0530

    fix: build and gitignore.

--- .docusaurus/client-modules.js
+++
@@ -1,5 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/sanchaymittal/Documents/GitHub/matic-docs/src/css/custom.css"),
-];
